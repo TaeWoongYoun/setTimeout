fb993b659031eab0e0da9dee6bcd411be4d4becb
xodnd0507 <24457545yong@gmail.com>
2024-02-18 21:52:00 +0900

    Test : study update

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+setTimeout(function(){}, 1000)
+
+function 함수(){
+    console.log('안녕')
+}
+
+
+
+// setInterval x초마다 이 코드를 실행해주세요
+// setInterval(function(){실행할코드},ms)
+
+
+//이렇게도 사용 가능
+// setTimeout(함수, 1000)
+// function 함수(){
+//     console.log('안녕')
+// }
+
+// addEventListener(function(){})
+// -> addEventListener(함수)
+// function 함수(){
+//     console.log('안녕')
