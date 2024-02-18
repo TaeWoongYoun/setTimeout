9bff807f7866fd2c7cba7cdc69713c1676b75406
xodnd0507 <24457545yong@gmail.com>
2024-02-18 22:27:02 +0900

    Test : homework clear!

--- index.js
+++ index.js
@@ -8,14 +8,15 @@
 // 만약에 span태그 안에 있는 글자가 0이 된다면 alert박스를 hide로 삭제해주세요.
 
 
-setInterval(span,1000)
-// function span(){
-//     document.getElementById('span').innerHTML -= 1 ;
-// }
+setInterval(span,1000);
+function span(){
+    document.getElementById('span').innerHTML -= 1 ;
 
-if(span == 0){
-    $('.alert').hide();
+    if(document.getElementById('span').innerHTML == 0){
+        $('.alert').hide();
+    }
 }
+
 
 
 // setInterval x초마다 이 코드를 실행해주세요
