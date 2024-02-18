aa91bfe5c7f4a929d433fe7601dad2caf1ba96c7
xodnd0507 <24457545yong@gmail.com>
2024-02-18 22:14:51 +0900

    Chore : 글자 변경 성공

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
+// setTimeout(function(){}, 1000)
+
+// function 함수(){
+//     console.log('안녕')
+// }
+
+// 1초마다 span태그 안에 있는 속성을 1씩 감소시켜 주세요
+// 만약에 span태그 안에 있는 글자가 0이 된다면 alert박스를 hide로 삭제해주세요.
 
 
-setTimeout(function(){}, 1000)
+setInterval(span,1000)
+// function span(){
+//     document.getElementById('span').innerHTML -= 1 ;
+// }
 
-function 함수(){
-    console.log('안녕')
+if(span == 0){
+    $('.alert').hide();
 }
-
 
 
 // setInterval x초마다 이 코드를 실행해주세요
